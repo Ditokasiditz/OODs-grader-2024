84be4b283044256008e2ec504c40d38fa47feff7
Siddiq <kasidit.samad@gmail.com>
2024-07-10 00:08:46 +0700

    finish function

--- function.py
+++ function.py
@@ -1,9 +1,16 @@
 def odd_list(al):
-    return None
+    new_odd_list = []
+    for i in al :
+        if i%2 == 1 :
+            new_odd_list.append(i)
+    
+    return new_odd_list 
+
+
 
 
 print(" ***Function Odd List***")
 ls = [int(e) for e in input("Enter list numbers : ").split()]
-print(ls)
+
 opls = odd_list(ls)
 print("Input list : ", ls, "\nOutput list : ", opls)
