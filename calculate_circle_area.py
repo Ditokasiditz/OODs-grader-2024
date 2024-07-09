6b20fbf6bb488f6efe80aa9ad265e788b1e77da5
Siddiq <kasidit.samad@gmail.com>
2024-07-09 12:51:25 +0700

    Create file1 python

---
+++ calculate_circle_area.py
@@ -0,0 +1,9 @@
+# เขียนโปรแกรม Python ซึ่งรับ input เป็นรัศมีของวงกลม จากนั้นคำนวณพื้นที่และแสดงผล
+# กำหนดให้
+# pi = 3.14159
+
+pi = 3.14159
+radius = float(input("r="))
+
+area = pi*radius*radius
+print("Area="+str(area))
